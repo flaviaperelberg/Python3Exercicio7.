c9883cb24683030fb35ed172880083afc1efff45
Flávia <113394221+flaviaperelberg@users.noreply.github.com>
2022-09-27 16:55:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Faça um programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário. 
+
+print('Bem-vindos(as) à calculadora que irá te dar em metros a área e em seguida o dobro da área de um quadrado!\n')
+
+lado=float(input('Insira em metros o tamanho da lateral do quadrado: \n'))
+
+area=lado*lado
+
+dobro=area*2
+
+print('A área do quadrado equivale:\n', area)
+
+print('O dobro da área equivale: \n', dobro)
